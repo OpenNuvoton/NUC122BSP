6a4ac51830068a8b7332cd96665467385746b7ee
MS20 YJChen5 <YJCHEN5@nuvoton.com>
2021-02-24 18:35:54 +0800

    USBD: Fix UART TX FIFO control issue.
    Change-Id: Icd2ca31c557a6a2328cb5256f845c459deb0918f

--- SampleCode/StdDriver/USBD_VCOM/main.c
+++ SampleCode/StdDriver/USBD_VCOM/main.c
@@ -148,7 +148,7 @@ void UART0_IRQHandler(void)
     if(u32IntStatus & 0x2 /* THRE_IF */)
     {
 
-        if(comTbytes)
+        if(comTbytes && (UART0->IER & UART_IER_THRE_IEN_Msk))
         {
             /* Fill the TX FIFO */
             size = comTbytes;
@@ -235,9 +235,7 @@ void VCOM_TransferData(void)
             if(comThead >= TXBUFSIZE)
                 comThead = 0;
 
-            __set_PRIMASK(1);
             comTbytes--;
-            __set_PRIMASK(0);
 
             /* Enable TX Empty Interrupt. (Trigger first one) */
             UART0->IER |= UART_IER_THRE_IEN_Msk;
